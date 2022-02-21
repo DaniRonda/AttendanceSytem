2b716d640ea1c4e755bef115e3a70fe71c8fcf8b
DaniRonda <dani68h5@easv365.dk>
2022-02-21 13:06:19 +0100

    :)

--- src/easv/dk/Main.java
+++ src/easv/dk/Main.java
@@ -1,8 +1,25 @@
 package easv.dk;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
+            launch();
+        }
+        @Override
+
+        public void start(Stage stage) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("GUI/View/MainScreen.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("Welcome :)");
+            stage.setScene(scene);
+            stage.show();
+        }
     }
-}
--- src/easv/dk/gui/controller/MainScreenController.java
+++ src/easv/dk/gui/controller/MainScreenController.java
@@ -1,4 +1,21 @@
 package easv.dk.gui.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainScreenController {
+    public void openTeacherView(KeyEvent e) throws IOException {
+
+    }
+
+    public void openStudentView(MouseEvent mouseEvent) throws IOException {
+
+    }
 }
+
