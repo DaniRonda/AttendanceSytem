453d5daf54e49e7258a90c81e93933896c9c5019
Mareike0308 <mare1702@easv365.dk>
2022-02-24 23:12:21 +0100

    areachart

--- src/easv/dk/gui/controller/StudentStatisticsController.java
+++ src/easv/dk/gui/controller/StudentStatisticsController.java
@@ -1,4 +1,31 @@
 package easv.dk.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
 public class StudentStatisticsController {
+
+    @FXML
+    public AreaChart <Number, Number> chart;
+
+    public void showChart(ActionEvent actionEvent) {
+
+        Axis<Number> xAxis = chart.getXAxis();
+        xAxis.setLabel("Date");
+
+        Axis <Number> yAxis = chart.getYAxis();
+        yAxis.setLabel("Percentage");
+
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+        series.setName("Attendance");
+
+        series.getData().add(new XYChart.Data<>(1, 90));
+        series.getData().add(new XYChart.Data<>(2, 85));
+    }
+
 }
